6320d7196cf97fe3c8f626630603d21be90c858c
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2025-04-27 18:15:23 +0100

    1.0.2 fix exports I think?

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,3 @@
-// Reexport your entry components here
 export * from './components/DiscordMessage.svelte'
+export * from './components/DiscordMessageContent.svelte'
+export * from './components/DiscordMessageImage.svelte'
